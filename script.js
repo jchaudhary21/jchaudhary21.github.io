dfec12b61d8b2c9bacb40fa37f0581eeae8c48b4
JC <jayeshchaudhary21jan@gmail.com>
2022-12-04 20:37:25 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,194 @@
+window.onload = function(){
+
+    document.getElementById("discord").onmouseover     = function() {mouseOver_discord()};
+    document.getElementById("discord").onmouseout      = function() {mouseOut()};
+
+    document.getElementById("instagram").onmouseover   = function() {mouseOver_instagram()};
+    document.getElementById("instagram").onmouseout    = function() {mouseOut()};
+
+    document.getElementById("youtube").onmouseover     = function() {mouseOver_youtube()};
+    document.getElementById("youtube").onmouseout      = function() {mouseOut()};
+    
+    document.getElementById("linkedin").onmouseover    = function() {mouseOver_linkedin()};
+    document.getElementById("linkedin").onmouseout     = function() {mouseOut()};
+
+    document.getElementById("wiki").onmouseover        = function() {mouseOver_wiki()};
+    document.getElementById("wiki").onmouseout         = function() {mouseOut()};
+
+    document.getElementById("website").onmouseover     = function() {mouseOver_website()};
+    document.getElementById("website").onmouseout      = function() {mouseOut()};
+
+    document.getElementById("github").onmouseover      = function() {mouseOver_github()};
+    document.getElementById("github").onmouseout       = function() {mouseOut()};
+
+    document.getElementById("whatsapp").onmouseover    = function() {mouseOver_whatsapp()};
+    document.getElementById("whatsapp").onmouseout     = function() {mouseOut()};
+
+    document.getElementById("twitter").onmouseover     = function() {mouseOver_twitter()};
+    document.getElementById("twitter").onmouseout      = function() {mouseOut()};
+
+    
+    function mouseOver_discord() {
+ 
+    document.getElementById("instagram").style.filter       = "blur(10px)";
+    document.getElementById("youtube").style.filter         = "blur(10px)";
+    document.getElementById("linkedin").style.filter        = "blur(10px)";
+    document.getElementById("wiki").style.filter            = "blur(10px)";
+    document.getElementById("website").style.filter         = "blur(10px)";
+    document.getElementById("twitter").style.filter         = "blur(10px)";
+    document.getElementById("whatsapp").style.filter        = "blur(10px)";
+    document.getElementById("github").style.filter          = "blur(10px)";
+    document.getElementById("documentation").style.filter   = "blur(10px)";
+    document.getElementById("projects").style.filter        = "blur(10px)";
+    document.getElementById("txt_discord").style.visibility        = "visible";
+  }
+
+  function mouseOver_website() {
+ 
+    document.getElementById("instagram").style.filter       = "blur(10px)";
+    document.getElementById("youtube").style.filter         = "blur(10px)";
+    document.getElementById("linkedin").style.filter        = "blur(10px)";
+    document.getElementById("wiki").style.filter            = "blur(10px)";
+    document.getElementById("discord").style.filter         = "blur(10px)";
+    document.getElementById("twitter").style.filter         = "blur(10px)";
+    document.getElementById("whatsapp").style.filter        = "blur(10px)";
+    document.getElementById("github").style.filter          = "blur(10px)";
+    document.getElementById("connect_with_us").style.filter   = "blur(10px)";
+    document.getElementById("projects").style.filter        = "blur(10px)";
+    document.getElementById("txt_website").style.visibility        = "visible";
+  }
+
+  function mouseOver_instagram() {
+ 
+    document.getElementById("discord").style.filter         = "blur(10px)";
+    document.getElementById("youtube").style.filter         = "blur(10px)";
+    document.getElementById("linkedin").style.filter        = "blur(10px)";
+    document.getElementById("wiki").style.filter            = "blur(10px)";
+    document.getElementById("website").style.filter         = "blur(10px)";
+    document.getElementById("twitter").style.filter         = "blur(10px)";
+    document.getElementById("whatsapp").style.filter        = "blur(10px)";
+    document.getElementById("github").style.filter          = "blur(10px)";
+    document.getElementById("documentation").style.filter   = "blur(10px)";
+    document.getElementById("projects").style.filter        = "blur(10px)";
+    document.getElementById("txt_instagram").style.visibility        = "visible";
+
+  }
+
+  function mouseOver_twitter() {
+ 
+    document.getElementById("instagram").style.filter       = "blur(10px)";
+    document.getElementById("youtube").style.filter         = "blur(10px)";
+    document.getElementById("linkedin").style.filter        = "blur(10px)";
+    document.getElementById("wiki").style.filter            = "blur(10px)";
+    document.getElementById("website").style.filter         = "blur(10px)";
+    document.getElementById("discord").style.filter         = "blur(10px)";
+    document.getElementById("whatsapp").style.filter        = "blur(10px)";
+    document.getElementById("github").style.filter          = "blur(10px)";
+    document.getElementById("documentation").style.filter   = "blur(10px)";
+    document.getElementById("projects").style.filter        = "blur(10px)";
+    document.getElementById("txt_twitter").style.visibility        = "visible";
+  }
+
+  function mouseOver_github() {
+ 
+    document.getElementById("instagram").style.filter       = "blur(10px)";
+    document.getElementById("youtube").style.filter         = "blur(10px)";
+    document.getElementById("linkedin").style.filter        = "blur(10px)";
+    document.getElementById("wiki").style.filter            = "blur(10px)";
+    document.getElementById("website").style.filter         = "blur(10px)";
+    document.getElementById("twitter").style.filter         = "blur(10px)";
+    document.getElementById("whatsapp").style.filter        = "blur(10px)";
+    document.getElementById("discord").style.filter          = "blur(10px)";
+    document.getElementById("documentation").style.filter   = "blur(10px)";
+    document.getElementById("connect_with_us").style.filter        = "blur(10px)";
+    document.getElementById("txt_github").style.visibility        = "visible";
+  }
+
+  function mouseOver_linkedin() {
+ 
+    document.getElementById("instagram").style.filter       = "blur(10px)";
+    document.getElementById("youtube").style.filter         = "blur(10px)";
+    document.getElementById("discord").style.filter        = "blur(10px)";
+    document.getElementById("wiki").style.filter            = "blur(10px)";
+    document.getElementById("website").style.filter         = "blur(10px)";
+    document.getElementById("twitter").style.filter         = "blur(10px)";
+    document.getElementById("whatsapp").style.filter        = "blur(10px)";
+    document.getElementById("github").style.filter          = "blur(10px)";
+    document.getElementById("documentation").style.filter   = "blur(10px)";
+    document.getElementById("projects").style.filter        = "blur(10px)";
+    document.getElementById("txt_linkedin").style.visibility        = "visible";
+  }
+
+  function mouseOver_whatsapp() {
+ 
+    document.getElementById("instagram").style.filter       = "blur(10px)";
+    document.getElementById("youtube").style.filter         = "blur(10px)";
+    document.getElementById("linkedin").style.filter        = "blur(10px)";
+    document.getElementById("wiki").style.filter            = "blur(10px)";
+    document.getElementById("website").style.filter         = "blur(10px)";
+    document.getElementById("twitter").style.filter         = "blur(10px)";
+    document.getElementById("discord").style.filter        = "blur(10px)";
+    document.getElementById("github").style.filter          = "blur(10px)";
+    document.getElementById("documentation").style.filter   = "blur(10px)";
+    document.getElementById("projects").style.filter        = "blur(10px)";
+    document.getElementById("txt_whatsapp").style.visibility        = "visible";
+  }
+
+  function mouseOver_youtube() {
+ 
+    document.getElementById("instagram").style.filter       = "blur(10px)";
+    document.getElementById("discord").style.filter         = "blur(10px)";
+    document.getElementById("linkedin").style.filter        = "blur(10px)";
+    document.getElementById("wiki").style.filter            = "blur(10px)";
+    document.getElementById("website").style.filter         = "blur(10px)";
+    document.getElementById("twitter").style.filter         = "blur(10px)";
+    document.getElementById("whatsapp").style.filter        = "blur(10px)";
+    document.getElementById("github").style.filter          = "blur(10px)";
+    document.getElementById("documentation").style.filter   = "blur(10px)";
+    document.getElementById("projects").style.filter        = "blur(10px)";
+    document.getElementById("txt_youtube").style.visibility        = "visible";
+  }
+
+  function mouseOver_wiki() {
+ 
+    document.getElementById("instagram").style.filter       = "blur(10px)";
+    document.getElementById("youtube").style.filter         = "blur(10px)";
+    document.getElementById("linkedin").style.filter        = "blur(10px)";
+    document.getElementById("discord").style.filter            = "blur(10px)";
+    document.getElementById("website").style.filter         = "blur(10px)";
+    document.getElementById("twitter").style.filter         = "blur(10px)";
+    document.getElementById("whatsapp").style.filter        = "blur(10px)";
+    document.getElementById("github").style.filter          = "blur(10px)";
+    document.getElementById("connect_with_us").style.filter   = "blur(10px)";
+    document.getElementById("projects").style.filter        = "blur(10px)";
+    document.getElementById("txt_wiki").style.visibility        = "visible";
+  }
+
+  function mouseOut() {
+ 
+    document.getElementById("discord").style.filter        = "blur(0px)";
+    document.getElementById("instagram").style.filter      = "blur(0px)";
+    document.getElementById("youtube").style.filter        = "blur(0px)";
+    document.getElementById("linkedin").style.filter       = "blur(0px)";
+    document.getElementById("wiki").style.filter           = "blur(0px)";
+    document.getElementById("website").style.filter        = "blur(0px)";
+    document.getElementById("twitter").style.filter        = "blur(0px)";
+    document.getElementById("whatsapp").style.filter       = "blur(0px)";
+    document.getElementById("github").style.filter         = "blur(0px)";
+    document.getElementById("documentation").style.filter  = "blur(0px)";
+    document.getElementById("projects").style.filter       = "blur(0px)";
+    document.getElementById("connect_with_us").style.filter        = "blur(0px)";
+    document.getElementById("txt_discord").style.visibility        = "hidden";
+    document.getElementById("txt_instagram").style.visibility      = "hidden";
+    document.getElementById("txt_twitter").style.visibility        = "hidden";
+    document.getElementById("txt_whatsapp").style.visibility       = "hidden";
+    document.getElementById("txt_youtube").style.visibility        = "hidden";
+    document.getElementById("txt_linkedin").style.visibility       = "hidden";
+    document.getElementById("txt_website").style.visibility        = "hidden";
+    document.getElementById("txt_wiki").style.visibility           = "hidden";
+    document.getElementById("txt_github").style.visibility         = "hidden";
+
+
+}
+
+}
